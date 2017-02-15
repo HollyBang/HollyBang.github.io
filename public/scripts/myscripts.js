2c8dd504916c764fda929323521bb441b1cad752
Andrey Barchuk <mazafakand@gmail.com>
2017-02-15 11:35:58 +0200

    added validation

--- public/scripts/myscripts.js
+++ public/scripts/myscripts.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
 	
 
 
-
+	//клик по тексту который не помещается в блок
 	var heightBlock = 150;
 	$(document).on('click', function (ev) {
 
@@ -64,9 +64,9 @@ $(document).ready(function(){
 
 		} else if (!$(target).hasClass(".items__text")) {
 			container.removeClass('background-change');
-				container.animate({
-					height: heightBlock
-				}, 400);
+			container.animate({
+				height: heightBlock
+			}, 400);
 		};
 
 		
@@ -89,5 +89,79 @@ $(document).ready(function(){
 		}, 400);
 		return false;
 	});
-})
 
+//валидация формы
+$("#form").validate({
+	rules: {
+		username: {
+			minlength: 0,
+			maxlength: 20,
+			required: true
+		},
+		email: {
+			email: true,
+			required: true
+		}
+
+	},
+	messages: {
+		username: {
+			email: "",
+			required: ""
+		},
+		email: {
+			email: "",
+			required: ""
+		}
+	},
+	highlight: function(element) {
+      $(element).closest(".form-control").removeClass("has-success").addClass("has-error").parents('form.animate-form').addClass("animateShake");
+    },
+    unhighlight: function(element) {
+      $(element).closest(".form-control").removeClass("has-error").addClass("has-success");
+    }
+  });
+
+  // $('.submit input').click(function() {
+  //   $('#form.animated').removeClass('animateShake');
+  //   if ($("#form").valid()) {
+  //     $("#form").addClass("success");
+  //   } else {
+  //     $("#form").removeClass("success").addClass("invalid");
+  //     $(this).addClass("disabled");
+  //   }
+
+  //   $("#form.invalid input").on("keyup blur", function() {
+  //     if ($("#form").valid()) {
+  //       $(".submit input").removeClass("disabled");
+  //       $("#form").removeClass("invalid");
+  //     } else {
+  //       $(".submit input").addClass("disabled");
+  //     }
+  //   });
+  // });
+});
+
+//модальное окно
+$(function() {
+
+
+	$('#contact').click(function() {
+		$('#contactForm').fadeToggle();
+		$('.bg_layer').show();
+	})
+	$(document).mouseup(function (e) {
+		var container = $("#contactForm");
+
+		if (!container.is(e.target)
+			&& container.has(e.target).length === 0)
+		{
+			container.fadeOut();
+			$('.bg_layer').fadeOut();
+		}
+	});
+
+});
+
+
+// $(element).closest(".form").addClass("animateShake");
